e334d84e5a79485cca6b27f558190cbc5cb483b9
anjankavyesh <anjankavyeshpagadala@gmail.com>
2020-07-20 02:11:53 +0530

    Goal_8

--- RDIP/Codes/expfile.js
+++ RDIP/Codes/expfile.js
@@ -82,6 +82,7 @@ if (selection.value === "english") {
      let changeWords = Math.floor(Math.random()*englishArray.length)
     document.getElementById("fn3").innerHTML = ""
     document.getElementById("fn4").innerHTML = ""
+    document.getElementById("fn5").innerHTML = ""
     valueToBeDisplay=""
     $("#hidefn").hide()
     $("#hideFn1").hide()
@@ -161,9 +162,10 @@ else if (selection.value === "hindi") {
    var randomizeWords = Math.floor(Math.random()*hindiArray.length)
     document.getElementById("fn3").innerHTML = ""
     document.getElementById("fn4").innerHTML = ""
+    document.getElementById("fn5").innerHTML = ""
     valueToBeDisplay = "";
     $("#hidefn").hide()
-    
+    $("#hideFn1").hide()
     let gettingValue = hindiArray[changeWords][0];
     k = randomizeTheValue(gettingValue);
     startingValue = 0;
@@ -182,6 +184,7 @@ else if (selection.value === "hindi") {
     document.getElementById("val").innerHTML = ""
     document.getElementById("fn3").innerHTML = ""
     document.getElementById("fn4").innerHTML = ""
+    document.getElementById("fn5").innerHTML = ""
     $("#hidefn").hide()
     $("#hideFn1").hide()
     alert("Please choose any language")
@@ -193,7 +196,102 @@ function reset() {
     }
     document.getElementById("fn3").innerHTML = ""
     document.getElementById("fn4").innerHTML = ""
+    document.getElementById("fn5").innerHTML = ""
     valueToBeDisplay = ""
     startingValue=0;
+    $("#hidefn").hide()
     $("#hideFn1").hide()
 }
+let answerKeyValue = ['John ate an apple before afternoon',
+'before afternoon John ate an apple',
+'John before afternoon ate an apple',
+'some students like to study in the night',
+'at night some students like to study'
+    ,'John and Mary went to church',
+    'Mary and John went to church',
+    'John went to church after eating', 
+    'after eating John went to church',
+    'John after eating went to church',
+    'did he go to market',
+    'he did go to market',
+    'the woman who called my sister sells cosmetics',
+    'the woman who sells cosmetics called my sister',
+    'my sister who sells cosmetics called the woman',
+    'my sister who called the woman sells cosmetics',
+    'John goes to the library and studies', 'John studies and goes to the library',
+    'John ate an apple so did she', 'she ate an apple so did John',
+    'the teacher returned the book after she noticed the error',
+    'the teacher noticed the error after she returned the book',
+    'after the teacher returned the book she noticed the error',
+    'after the teacher noticed the error she returned the book',
+    'she returned the book after the teacher noticed the error',
+    'she noticed the error after the teacher returned the book',
+    'after she returned the book the teacher noticed the error',    
+    'after she noticed the error the teacher returned the book',
+    'I told her that I bought a book yesterday',    
+    'I told her yesterday that I bought a book',
+    'yesterday I told her that I bought a book', 
+    'I bought a book that I told her yesterday',
+    'I bought a book yesterday that I told her',
+    'yesterday I bought a book that I told her',
+    'राम और श्याम बाजार गयें',
+    'राम और श्याम गयें बाजार',
+    'बाजार गयें राम और श्याम',
+    'गयें बाजार राम और श्याम',
+    'राम सोया और श्याम भी',
+    'श्याम सोया और राम भी',
+    'सोया श्याम और राम भी',
+    'सोया राम और श्याम भी',
+    'मैंने उसे बताया कि राम सो रहा है',
+    'मैंने उसे बताया कि सो रहा है राम',
+    'उसे मैंने बताया कि राम सो रहा है',
+    'उसे मैंने बताया कि सो रहा है राम',
+    'मैंने बताया उसे कि राम सो रहा है',
+    'मैंने बताया उसे कि सो रहा है राम',
+    'उसे बताया मैंने कि राम सो रहा है',
+    'उसे बताया मैंने कि सो रहा है राम',
+    'बताया मैंने उसे कि राम सो रहा है',
+    'बताया मैंने उसे कि सो रहा है राम',
+    'बताया उसे मैंने कि राम सो रहा है',
+    'बताया उसे मैंने कि सो रहा है राम',
+    'राम खाकर सोया',
+    'खाकर राम सोया',
+    'राम सोया खाकर',
+    'खाकर सोया राम',
+    'सोया राम खाकर',
+    'सोया खाकर राम',
+    'बिल्लियों को मारकर कुत्ता सो गया',
+    'मारकर बिल्लियों को कुत्ता सो गया',
+    'बिल्लियों को मारकर सो गया कुत्ता',
+    'मारकर बिल्लियों को सो गया कुत्ता',
+    'कुत्ता सो गया बिल्लियों को मारकर',
+    'कुत्ता सो गया मारकर बिल्लियों को',
+    'सो गया कुत्ता बिल्लियों को मारकर',
+    'सो गया कुत्ता मारकर बिल्लियों को',
+    'एक लाल किताब वहाँ है',
+    'एक लाल किताब है वहाँ',
+    'वहाँ है एक लाल किताब',
+    'है वहाँ एक लाल किताब',
+    'एक बड़ी सी किताब वहाँ है',
+    'एक बड़ी सी किताब है वहाँ',
+    'बड़ी सी एक किताब वहाँ है',
+    'बड़ी सी एक किताब है वहाँ',
+    'वहाँ है एक बड़ी सी किताब',
+    'वहाँ है बड़ी सी एक किताब',
+    'है वहाँ एक बड़ी सी किताब',
+    'है वहाँ बड़ी सी एक किताब'
+     ];
+function checkCorrectness(){
+    let storeValue = valueToBeDisplay.trim()
+    for(let i =0;i<answerKeyValue.length;i++){
+        let temporary = answerKeyValue[i]
+        let localValue = temporary.localeCompare(storeValue)
+        if(localValue === 0){
+            document.getElementById("fn5").innerHTML = "Right answer!!!"
+            document.getElementById("fn5").style.color = "green"
+            return  document.getElementById("fn5").innerHTML = "Right answer!!!"
+            }
+            document.getElementById("fn5").innerHTML = "Wrong answer!!!"
+            document.getElementById("fn5").style.color = "red"
+        }
+ }
